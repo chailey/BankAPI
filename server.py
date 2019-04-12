9acced0ddc731dc3cdb31e83385c331969ec6cda
Chris Hailey <chris.hailey8@gmail.com>
2019-04-12 01:19:13 -0700

    Added post features for User

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ class Account(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from Account")
-        return {'Account': [i[0] for i in query.cursor.fetchall()]}
+        return jsonify({'Account': [i[0] for i in query.cursor.fetchall()]})
     def post(self):
         pass
 
@@ -20,7 +20,7 @@ class CreditCard(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from CreditCard")
-        return {'CreditCard': [i[0] for i in query.cursor.fetchall()]}
+        return jsonify({'CreditCard': [i[0] for i in query.cursor.fetchall()]})
     def post(self):
         pass
 
@@ -28,29 +28,56 @@ class Transaction(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from Transaction")
-        return {'Transaction': [i[0] for i in query.cursor.fetchall()]}
+        return jsonify({'Transaction': [i[0] for i in query.cursor.fetchall()]})
     def post(self):
-        pass
+        conn = db_connect.connect()
+        transaction_id = request.json['transaction_id']
+        student_id = request.json['student_id']
+        account_number = request.json['account_number']
+        amount = request.json['amount']
+        query = conn.execute("insert into Transaction"
+                             "(transaction_id, student_id, account_number, amount,"
+                             " acquiring_account_number) values (?,?,?,?,?)",
+                             transaction_id, student_id, account_number, amount, 0)
+        return {'status': 'success'}
+
 
 class User(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from User")
-        return {'User': [i[0] for i in query.cursor.fetchall()]}
+        return jsonify({'User': [i[0] for i in query.cursor.fetchall()]})
     def post(self):
-        pass
+        conn = db_connect.connect()
+        student_id = request.json['student_id']
+        address = request.json['address']
+        email = request.json['email']
+        first_name = request.json['first_name']
+        last_name = request.json['last_name']
+        query = conn.execute("insert into User"
+                             "(student_id, address, email, first_name,"
+                             " last_name) values (?,?,?,?,?)",
+                             student_id, address, email, first_name, last_name)
+        return {'status': 'success'}
+
+
+class UserID(Resource):
+    def get(self, student_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from User where student_id = %d" %int(student_id))
+        return jsonify({'User': [dict(zip(tuple(query.keys()) ,i)) for i in query.cursor]})
 
 class CreditCardTypes(Resource):
     def get(self):
         conn = db_connect.connect()
-        query = conn.execute("select type_num, type from CreditCardTypes")
-        return {'CreditCardTypes': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute("select * from CreditCardTypes")
+        return jsonify({'CreditCardTypes': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]})
 
 class AccountTypes(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from AccountTypes")
-        return {'AccountTypes': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor.fetchall()]}
+        return jsonify({'AccountTypes': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor.fetchall()]})
 
 @app.route('/')
 def hello_world():
@@ -60,6 +87,7 @@ def hello_world():
 api.add_resource(CreditCardTypes, '/CreditCardTypes')
 api.add_resource(AccountTypes, '/AccountTypes')
 api.add_resource(User, '/User')
+api.add_resource(UserID, '/User/<student_id>')
 
 if __name__ == '__main__':
      app.run()
